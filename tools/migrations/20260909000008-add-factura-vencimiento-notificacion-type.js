4e30f23ae73d92cc8ca2a59bc75fdbe40570ccdc
carrillo1919 <carloscarrillo119@gmail.com>
2026-09-10 00:42:33 -0400

    chore: excluir tools/ del repositorio de despliegue

--- tools/migrations/20260909000008-add-factura-vencimiento-notificacion-type.js
+++
@@ -1,9 +0,0 @@
-export const up = async (queryInterface) => {
-  await queryInterface.sequelize.query(
-    "ALTER TYPE \"enum_notificacion_logs_tipo\" ADD VALUE IF NOT EXISTS 'factura_vencimiento';"
-  );
-};
-
-export const down = async () => {
-  // PostgreSQL no permite eliminar un valor ENUM de forma segura.
-};
